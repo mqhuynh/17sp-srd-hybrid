54e02da93950e094e6db50020ae382091f2382a7
Damodar Dahal <underscoredam@gmail.com>
2017-04-22 21:09:29 -0500

    respond more fields

--- server/api.php
+++ server/api.php
@@ -2,6 +2,11 @@
 header('Access-Control-Allow-Origin: *');  
 
 echo json_encode(
-    array('data'=> $all_data,
-        'types'=> $datatype));
+    array(
+        'data'=> $all_data,
+        'types'=> $datatype,
+        'search_keys' => $search_keys,
+        'fieldname' => $fieldname,
+        'name_keys' => $name_keys
+));
 
