e7a0ede4bb2aedb0e4aed920cd917ea8bad5dd6c
Damodar Dahal <underscoredam@gmail.com>
2017-04-28 00:09:46 -0500

    implement loader

--- client/src/pages/search/search.ts
+++ client/src/pages/search/search.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {ListPage} from '../list/list'
-import { AutoCompleteModule } from 'ionic2-auto-complete';
-import { CompleteTestService } from '../../providers/complete-test-service';
+import {AutoCompleteModule} from 'ionic2-auto-complete';
+import {CompleteTestService} from '../../providers/complete-test-service';
 import {ApiService} from '../../providers/api-service'
+import {LoadingController} from 'ionic-angular';
 
 @Component({
   selector: 'page-search',
@@ -13,21 +14,32 @@ export class SearchPage {
   selectData = [];
   selectedKey: string = '';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public completeTestService: CompleteTestService, public api: ApiService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public completeTestService: CompleteTestService, public api: ApiService, public loading: LoadingController) {
+    const loaderBox = this.loading.create({
+      content: 'Getting latest entries...',
+    });
+
     const loader = () => {
-      if(this.api.getData() === null){
+      if (this.api.getData() === null) {
         return setTimeout(loader, 200);
       }
 
       this.dataLoaded();
+      loaderBox.dismiss();
 
       //testing purpose only
-      setTimeout(() => {this.searchItemSelected(this.completeTestService.getResults('a')[0]);}, 100);
+      setTimeout(() => {
+        // this.searchItemSelected(this.completeTestService.getResults('a')[0]);
+      }, 100);
     };
+
+    loaderBox.present();
+
+
     loader();
   }
 
-  dataLoaded(){
+  dataLoaded() {
     const data = this.api.getData();
 
     this.selectData = Object.keys(data.search_keys)
@@ -37,23 +49,23 @@ export class SearchPage {
         value: data.fieldname[key]
       }));
 
-    if(this.selectData && this.selectData.length>0){
+    if (this.selectData && this.selectData.length > 0) {
       this.selectedKey = this.selectData[0];
       this.searchModeChanged(this.selectedKey);
     }
   }
 
-  gotoListPage(item: any){
+  gotoListPage(item: any) {
     this.navCtrl.push(ListPage, item);
 
   }
 
-  searchItemSelected(item){
+  searchItemSelected(item) {
     console.log('item selected', item);
     this.gotoListPage(item);
   }
 
-  searchModeChanged(e){
+  searchModeChanged(e) {
     this.completeTestService.updateSearchMode(e.key);
   }
 
