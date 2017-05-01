9310b197de0ef5f0012a16d7cccd707eb1c89c72
Damodar Dahal <underscoredam@gmail.com>
2017-05-01 18:53:02 -0500

    Annotate ApiService and CompleteTestService

--- client/src/providers/api-service.ts
+++ client/src/providers/api-service.ts
@@ -2,29 +2,38 @@ import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
- Generated class for the ApiService provider.
 
- See https://angular.io/docs/ts/latest/guide/dependency-injection.html
- for more info on providers and Angular 2 DI.
+/**
+ * Provider used to communicate with the PHP web API
+ *
+ * loadData() is called (once) when the ionic application is loaded. to get the API data as-is, call getData().
  */
 @Injectable()
 export class ApiService {
-  static readonly ADDRESS: string = "http://gdv.local/api.php";
+
+  // the server address
+  static readonly ADDRESS: string = "http://localhost/api.php";
+
+  //server data is stored in this variable
   static data: any = null;
+
 
   constructor(public http: Http) {
   }
 
   public loadData() {
     this.http.get(ApiService.ADDRESS)
-      .map((response) => response.json())
+      .map((response) => response.json()) //convert data to JSON
       .subscribe(data => {
         ApiService.data = data;
         console.log('data loaded from API');
       });
   }
 
+  /**
+   * Return data from API as-is
+   * @returns {any}: the data
+   */
   public getData(){
     return ApiService.data;
   }
--- client/src/providers/complete-test-service.ts
+++ client/src/providers/complete-test-service.ts
@@ -4,28 +4,53 @@ import {Injectable} from "@angular/core";
 import 'rxjs/add/operator/map'
 import {ApiService} from './api-service'
 
+
+/**
+ * Used to create the "dropdown" options-list for ionic2-auto-complete module
+ *
+ * Used in conjunction with the ionic2-auto-complete module.
+ * Basically, it filters the data from ApiService into user provided query.
+ *
+ */
 @Injectable()
 export class CompleteTestService implements AutoCompleteService {
-  labelAttribute = "name";
+  // overridden, useless string
+  labelAttribute: string;
+
+  // the column of field in api data
   index = 0;
 
   constructor(protected api: ApiService) {
 
   }
+
+  /**
+   * Return matching items for the "dropdown" options
+   *
+   * @param keyword: whatever the user has entered
+   * @returns Array<any>: the filtered list of options
+   */
   getResults(keyword:string) {
     const data = this.api.getData();
     if(!data)return [];
 
-    const ret = Object.keys(data.data)
-      .filter($id => data.data[$id][this.index].toLowerCase().indexOf(keyword.toLowerCase())>=0)
-      .map($id => ({$id, value: data.data[$id], name: data.data[$id][this.index]}));
-
-    return ret;
+    //complex function to filter+map the API data to make it understandable by the ionic2-auto-complete module.
+    return Object.keys(data.data)
+      .filter($id => data.data[$id][this.index].toLowerCase().indexOf(keyword.toLowerCase()) >= 0) //filter matching elements
+      .map($id => ( //provide the output in an understandable format by the ionic2-auto-complete
+        {$id, value: data.data[$id], name: data.data[$id][this.index]}
+      ));
   }
 
-  updateSearchMode(mode: number){
+  /**
+   * Update the index to search by.
+   *
+   * This is triggered when the user switches the search mode in Search page
+   * @param index The index of the search item in API data
+   */
+  updateSearchMode(index: number){
     //accept index of search element
-    this.index = mode;
+    this.index = index;
 
   }
 }
